ce84f66ba85022f89692d00f83d10f53e7613cb4
User <User@LAB3-8>
2022-04-03 12:28:51 +0300

    Add project files.

---
+++ Demos/SkiaSharpFormsDemos/ChennyTestPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace SkiaSharpFormsDemos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChennyTestPage : ContentPage
+    {
+        public ChennyTestPage()
+        {
+            InitializeComponent();
+        }
+
+        private void chennyTestCanvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+            canvas.Clear();
+            #region board
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    SKPath path = new SKPath();
+                    path.MoveTo(j * 0.1f * info.Width, i * 0.1f * info.Height);
+                    path.LineTo((0.1f + j * 0.1f) * info.Width, i * 0.1f * info.Height);
+                    path.LineTo((0.1f + j * 0.1f) * info.Width, (0.1f + i * 0.1f) * info.Height);
+                    path.LineTo(j * 0.1f * info.Width, (0.1f + i * 0.1f) * info.Height);
+                    path.LineTo(j * 0.1f * info.Width, i * 0.1f * info.Height);
+                    path.Close();
+                    SKPaint fillPaint = new SKPaint
+                    {
+                        Style = SKPaintStyle.Fill,
+                        Color = SKColors.Goldenrod,
+                    };
+                    if ((i + j) % 2 == 0)
+                    {
+                        fillPaint = new SKPaint
+                        {
+                            Style = SKPaintStyle.Fill,
+                            Color = SKColors.LightGoldenrodYellow,
+                        };
+                    }
+                    int posNum = 10 * (9 - i) + (9 - j) + 1;
+                    if (i % 2 == 1)
+                        posNum = 10 * (9 - i) + j + 1;
+                    if(posNum == 100)
+                    {
+                        fillPaint = new SKPaint
+                        {
+                            Style = SKPaintStyle.Fill,
+                            Color = SKColors.Pink,
+                        };
+                    }
+                    canvas.DrawPath(path, fillPaint);
+                    
+                    string text = posNum.ToString();
+                    float textX = j * 0.1f * info.Width;
+                    float textY = (0.1f + i * 0.1f) * info.Height;
+                    SKPaint textPaint = new SKPaint
+                    {
+                        TextSize = 30,
+                        Color = SKColors.Black,
+                    };
+                    canvas.DrawText(text, textX, textY, textPaint);
+                }
+            }
+            #endregion
+            
+        }
+    }
+}
